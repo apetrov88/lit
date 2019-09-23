e45407bbdbd8527085193b9e558e738bac5f49b6
Alex <apetrov88@users.noreply.github.com>
2019-09-23 15:05:32 +0300

    f

--- app/controllers/lit/application_controller.rb
+++ app/controllers/lit/application_controller.rb
@@ -1,6 +1,6 @@
 module Lit
   class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    #skip_before_action :verify_authenticity_token
 
     unless respond_to?(:before_action)
       alias before_action before_filter
